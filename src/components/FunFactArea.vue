0c977f3db83e6488a85dd7d5397462b836db1231
movi008 <socialmedia.ovi@gmail.com>
2023-07-18 20:14:54 +0600

    Initial commit

---
+++ src/components/FunFactArea.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+</script>
+
+<template>
+<section class="fun-facts-area pt-100 pb-70">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-fun-fact">
+                    <div class="icon">
+                        <i class="fa fa-users"></i>
+                    </div>
+                    <h3>
+                        <span class="odometer" data-count="1020">00</span>
+                    </h3>
+                    <p>Usersr</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-fun-fact">
+                    <div class="icon">
+                        <i class="fa fa-heart"></i>
+                    </div>
+                    <h3>
+                        <span class="odometer" data-count="5679">00</span>
+                    </h3>
+                    <p>Happy Clients</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-fun-fact">
+                    <div class="icon">
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <h3>
+                        <span class="odometer" data-count="2660">00</span>
+                    </h3>
+                    <p>Reviews</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-sm-6">
+                <div class="single-fun-fact">
+                    <div class="icon">
+                        <i class="fa fa-download"></i>
+                    </div>
+                    <h3>
+                        <span class="odometer" data-count="6789">00</span>
+                    </h3>
+                    <p>App Downloads</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</template>    
+
+
+<style scoped>
+
+</style>
---
+++ src/components/MainBannerArea.vue
@@ -0,0 +1,69 @@
+<script setup>
+
+</script>
+
+<template>
+
+<div>
+    <div id="home"  class="main-banner">
+    <div class="d-table">
+        <div class="d-table-cell">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="banner-content">
+                            <h1>Best Mobile App Template For Your Business</h1>
+                            <p>App ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                            <div class="banner-holder">
+                                <a href="#">
+                                    <img src="/src/assets/img/store/1.png" alt="image">
+                                </a>
+                                <a href="#">
+                                    <img src="/src/assets/img/store/2.png" alt="image">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="banner-image">
+                            <img src="/src/assets/img/mobile.png" alt="image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="default-shape">
+        <div class="shape-1">
+            <img src="/src/assets/img/shape/1.png" alt="image">
+        </div>
+
+        <div class="shape-2 rotateme">
+            <img src="/src/assets/img/shape/2.png" alt="image">
+        </div>
+
+        <div class="shape-3">
+            <img src="/src/assets/img/shape/3.svg" alt="image">
+        </div>
+
+        <div class="shape-4">
+            <img src="/src/assets/img/shape/4.svg" alt="image">
+        </div>
+
+        <div class="shape-5">
+            <img src="/src/assets/img/shape/5.png" alt="image">
+        </div>
+    </div>
+</div>
+
+</div>
+
+</template>
+
+
+<style scoped>
+
+</style>
